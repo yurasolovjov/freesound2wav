d0b81e632faff9cffdea3619fd496ab51b5437df
yurasolovjov <yura.solovjov@mail.ru>
2018-08-21 12:30:25 +0300

    Added limit of page

--- main.py
+++ main.py
@@ -15,16 +15,22 @@ import freesound
 # Ключ доступа к Freesound API
 GEN_KEY_FREESOUND = str("PYqNLRWMA9URy55hSbzK5lGxJsgVJaBsvRx6giFG")
 
+# Ограничение на максимальное количество страниц
+LIMIT_PAGE = int(10)
+
 def main():
 
     parser = argparse.ArgumentParser(description="Options");
     parser.add_argument("-s", "--search", help="Keyword for search", action="append", default=None, nargs="*")
     parser.add_argument("-o", "--out", help="Output catalog", default="../freesound")
+    parser.add_argument("-l", "--lim_page", help="Limit of page", default=LIMIT_PAGE)
 
     args = parser.parse_args()
 
     search_tokens = list()
     keywords = args.search
+    lim_page_count =  args.lim_page
+
     output = os.path.normpath(args.out)
 
     for keyword in keywords:
@@ -60,14 +66,24 @@ def main():
             if not os.path.exists(output_catalog):
                 os.makedirs(output_catalog)
 
-            for sound in results:
-                try:
-                    sound.retrieve_preview(output_catalog)
-                    info = "Saved file: " + str(output_catalog) + str(sound.name)
-                    print(termcolor.colored(info, "green"))
-                except:
-                    info = str("Sound can`t be saved to " + str(output_catalog) + str(sound.name) )
-                    print(termcolor.colored(info, "red"))
+            page_count = int(0)
+
+            while True:
+                for sound in results:
+                    try:
+                        sound.retrieve_preview(output_catalog)
+                        info = "Saved file: " + str(output_catalog) + str(sound.name)
+                        print(termcolor.colored(info, "green"))
+                    except:
+                        info = str("Sound can`t be saved to " + str(output_catalog) + str(sound.name) )
+                        print(termcolor.colored(info, "red"))
+
+                page_count += 1
+
+                if not results.next or lim_page_count == page_count:
+                    break
+
+                results = results.next_page()
         except:
             print(termcolor.colored(" Search is failed ", "red"))
 
